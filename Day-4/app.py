2b9a08201c26132da84e6b61afa8c0561661bf6c
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-11-23 09:45:45 +0100

    added rock papper scissor game

--- Day-4/app.py
+++ Day-4/app.py
@@ -22,16 +22,78 @@ import random
 
 # * Treasure map
 
-row1 = ["⬜️", "⬜️", "⬜️"]
-row2 = ["⬜️", "⬜️", "⬜️"]
-row3 = ["⬜️", "⬜️", "⬜️"]
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
+# row1 = ["⬜️", "⬜️", "⬜️"]
+# row2 = ["⬜️", "⬜️", "⬜️"]
+# row3 = ["⬜️", "⬜️", "⬜️"]
+# map = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}")
 
-position = input("Where do you want to put the treasure? ")
+# position = input("Where do you want to put the treasure? ")
 
-column = int(position[0])
-row = int(position[1])
+# column = int(position[0])
+# row = int(position[1])
 
-map[row-1][column-1] = 'X'
-print(f"{row1}\n{row2}\n{row3}")
+# map[row-1][column-1] = 'X'
+# print(f"{row1}\n{row2}\n{row3}")
+
+
+# * Rock Paper  Scissors
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+posiibilities = [rock, paper, scissors]
+
+rand_num = random.randint(0, 2)
+
+computer_choice = posiibilities[rand_num]
+
+user_num = int(input(
+    'What do you choose? Type 0 for Rock, Type 1 for Paper, Type 2 for scissors: '))
+
+user_choice = posiibilities[user_num]
+
+if user_choice == rock and computer_choice == scissors:
+    print(user_choice, computer_choice, 'You win\n')
+
+elif user_choice == scissors and computer_choice == rock:
+    print(user_choice, computer_choice, 'You lose\n')
+
+elif user_choice == rock and computer_choice == paper:
+    print(user_choice, computer_choice, 'You lose\n')
+
+elif user_choice == paper and computer_choice == rock:
+    print(user_choice, computer_choice, 'You win\n')
+
+elif user_choice == scissors and computer_choice == paper:
+    print(user_choice, computer_choice, 'You win\n')
+
+elif user_choice == paper and computer_choice == scissors:
+    print(user_choice, computer_choice, 'You lose\n')
+elif user_choice == computer_choice:
+    print(user_choice, computer_choice, 'You\'re equal')
