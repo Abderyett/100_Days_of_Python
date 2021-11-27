d0060544c822845d9340c79c22c4656c63437bb7
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-11-27 09:30:29 +0100

    finalize Day-6

--- Day-6/app.py
+++ Day-6/app.py
@@ -1,26 +1,47 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def jump():
+
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+
+# while not at_goal():
+
+#     if wall_in_front():
+#         jump()
+
+#     else:
+#         move()
+
+
+# * Maze Solution
+
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
 
-def jump():
-
-    turn_left()
-    while wall_on_right():
-        move()
-    turn_right()
+while front_is_clear():
     move()
-    turn_right()
-    while front_is_clear():
-        move()
     turn_left()
-
-
 while not at_goal():
-
-    if wall_in_front():
-        jump()
-
-    else:
+    if right_is_clear():
+        turn_right()
         move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
