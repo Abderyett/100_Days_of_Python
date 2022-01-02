7596eb33f081f16a34751b3eaa03a1922206e59f
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-01-02 21:54:10 +0100

    start on input and money total in machine coffe app

--- Day-15/app.py
+++ Day-15/app.py
@@ -29,3 +29,16 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+
+drink = input("What would you like? (espresso/latte/cappuccino): ")
+print("Please insert coins.")
+
+quarter = int(input("how many quarters? : "))
+dimes = int(input("how many dimes? : "))
+nickles = int(input("how many nickles? : "))
+pennies = int(input("how many pennies? : "))
+
+total = 0.25 * quarter + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
+print(type(total))
+print(f"total {total}")
