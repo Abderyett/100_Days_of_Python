088b663febbc3d38fbef86ad50ac2a115ee0ed3f
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-01-03 22:31:27 +0100

    update change an change

--- Day-15/app.py
+++ Day-15/app.py
@@ -30,15 +30,20 @@ resources = {
     "coffee": 100,
 }
 
+while True:
+    drink = input("What would you like? (espresso/latte/cappuccino): ")
+    if drink == "report":
+        print(
+            f"Water {resources['water']}ml\nMilk {resources['milk']}ml\nCoffee {resources['coffee']}g"
+        )
+    elif drink == "espresso" or drink == "latte" or drink == "cappuccino":
+        print("Please insert coins.")
 
-drink = input("What would you like? (espresso/latte/cappuccino): ")
-print("Please insert coins.")
-
-quarter = int(input("how many quarters? : "))
-dimes = int(input("how many dimes? : "))
-nickles = int(input("how many nickles? : "))
-pennies = int(input("how many pennies? : "))
-
-total = 0.25 * quarter + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
-print(type(total))
-print(f"total {total}")
+        quarter = int(input("how many quarters? : "))
+        dimes = int(input("how many dimes? : "))
+        nickles = int(input("how many nickles? : "))
+        pennies = int(input("how many pennies? : "))
+        total = 0.25 * quarter + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
+        change = total - MENU[drink]["cost"]
+        print(f"Here is {change}$ in change")
+        print(f"Here is your {drink} ☕️, Enjoy! ")
