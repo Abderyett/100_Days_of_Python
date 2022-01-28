10347ad7c7fea3d0ae18961ba68007fde635068b
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-01-28 20:26:02 +0100

    Complete Day-20

--- Day-20/app.py
+++ Day-20/app.py
@@ -3,6 +3,7 @@ from turtle import Screen
 from snake import Snake
 
 screen = Screen()
+screen.listen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
@@ -10,11 +11,15 @@ screen.tracer(0)
 screen.title("Snake Game")
 
 snake = Snake()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 is_continnue_moving = True
 while is_continnue_moving:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.1)
 
     snake.move()
     # to imitate real snake move and be able to turn the head of snake and followed by body
--- Day-20/snake.py
+++ Day-20/snake.py
@@ -1,12 +1,19 @@
+from re import L
 from turtle import Turtle
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVING_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for pos in STARTING_POSITION:
@@ -23,4 +30,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].fd(20)
+        self.head.fd(MOVING_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
