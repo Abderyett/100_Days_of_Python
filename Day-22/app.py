8a8a39c92a38cf4fd3d179cde731e9d39de4e141
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-02-05 14:53:44 +0100

    Initilize Day-22/Library/Frameworks/Python.framework/Versions/3.9/bin/python3

--- Day-22/app.py
+++ Day-22/app.py
@@ -1,7 +1,17 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
+paddle = Paddle(350)
+paddle2 = Paddle(-350)
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.listen()
+
+
+screen.onkey(paddle.move_up, "Up")
+screen.onkey(paddle.move_down, "Down")
+
+
 screen.exitonclick()
