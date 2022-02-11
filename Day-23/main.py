6ffbb2b33ab02fa377770f3a129149225a52e830
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-02-11 19:42:47 +0100

    Create turtle and make it move

--- Day-23/main.py
+++ Day-23/main.py
@@ -5,8 +5,13 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+player = Player()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
--- Day-23/player.py
+++ Day-23/player.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move(self):
+
+        x_pos = self.xcor()
+        y_pos = self.ycor()
+        self.goto(x_pos, y_pos+MOVE_DISTANCE)
