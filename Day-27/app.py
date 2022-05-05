d279911fa3fe2fdba5e325d23055de592d77d7c0
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-05 07:56:59 +0100

    Start Day-27 exploring GUI and unlimited args

---
+++ Day-27/app.py
@@ -0,0 +1,9 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="First Label", font=("Monolisa", 24, "bold"))
+my_label.pack()
+window.mainloop()
---
+++ Day-27/playground.py
@@ -0,0 +1,7 @@
+# Unlimited positioned agruments
+def add(*args):
+    res = sum(list(args))
+    print(res)
+
+
+add(1, 2, 3, 4, 5, 6, 7, 8)
