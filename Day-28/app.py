6aebd6c0b2c75c1f0dfc3d24addda5a01c12d337
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-08 07:31:24 +0100

    Fix promodoro layout

--- Day-28/app.py
+++ Day-28/app.py
@@ -20,12 +20,24 @@ window = Tk()
 window.config(bg=YELLOW)
 window.title("Pomodoro")
 window.config(padx=100, pady=50)
-
 tomato_img = PhotoImage(file="./tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white",
                    font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=2, row=2)
+# Lablel
+label = Label(text="Timer", font=(FONT_NAME, 60, "bold"), fg=GREEN, bg=YELLOW)
+label.grid(column=2, row=1)
+
+label_check = Label(text="✓", font=(FONT_NAME, 40, "bold"),
+                    fg=GREEN, bg=YELLOW).grid(column=2, row=8)
+# Buttons
+
+start_btn = Button(text="Start", fg="white",
+                   highlightbackground=GREEN, cursor="hand", highlightthickness=0).grid(column=1, row=8)
+reset_btn = Button(text="reset", fg="white",
+                   highlightbackground=RED, cursor="hand", highlightthickness=0).grid(column=9, row=8)
+canvas.grid()
 
 window.mainloop()
