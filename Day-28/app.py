4c8c26bb8ae47f74876967231a77a1d1c571ee15
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-08 09:18:40 +0100

    set timeout for promodoro

--- Day-28/app.py
+++ Day-28/app.py
@@ -15,17 +15,29 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+
+def count_down(count):
+    canvas.itemconfig(text_timer, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.config(bg=YELLOW)
 window.title("Pomodoro")
 window.config(padx=100, pady=50)
+window.after(10)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white",
-                   font=(FONT_NAME, 35, "bold"))
+text_timer = canvas.create_text(100, 130, text="00:00", fill="white",
+                                font=(FONT_NAME, 35, "bold"))
+
+
 canvas.grid(column=2, row=2)
+
+count_down(10)
 # Lablel
 label = Label(text="Timer", font=(FONT_NAME, 60, "bold"), fg=GREEN, bg=YELLOW)
 label.grid(column=2, row=1)
