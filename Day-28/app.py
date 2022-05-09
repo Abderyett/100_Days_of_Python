fc5f787b32e464c2fcd103e35b4c6e1380226d4d
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-09 07:25:53 +0100

    set timeout working by second

--- Day-28/app.py
+++ Day-28/app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,11 +14,16 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_counter():
+    count_down(WORK_MIN*60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def count_down(count):
-    canvas.itemconfig(text_timer, text=count)
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    canvas.itemconfig(text_timer, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -27,7 +33,7 @@ window = Tk()
 window.config(bg=YELLOW)
 window.title("Pomodoro")
 window.config(padx=100, pady=50)
-window.after(10)
+
 tomato_img = PhotoImage(file="./tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_img)
@@ -37,9 +43,10 @@ text_timer = canvas.create_text(100, 130, text="00:00", fill="white",
 
 canvas.grid(column=2, row=2)
 
-count_down(10)
+
 # Lablel
-label = Label(text="Timer", font=(FONT_NAME, 60, "bold"), fg=GREEN, bg=YELLOW)
+label = Label(text="Timer", font=(
+    FONT_NAME, 60, "bold"), fg=GREEN, bg=YELLOW)
 label.grid(column=2, row=1)
 
 label_check = Label(text="✓", font=(FONT_NAME, 40, "bold"),
@@ -47,7 +54,7 @@ label_check = Label(text="✓", font=(FONT_NAME, 40, "bold"),
 # Buttons
 
 start_btn = Button(text="Start", fg="white",
-                   highlightbackground=GREEN, cursor="hand", highlightthickness=0).grid(column=1, row=8)
+                   highlightbackground=GREEN, cursor="hand", highlightthickness=0, command=start_counter).grid(column=1, row=8)
 reset_btn = Button(text="reset", fg="white",
                    highlightbackground=RED, cursor="hand", highlightthickness=0).grid(column=9, row=8)
 canvas.grid()
