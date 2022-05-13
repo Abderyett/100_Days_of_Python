bd199c8ee259fd8c19828f4242d1acacd4c6c2d4
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-13 19:35:37 +0100

    setup widgets and layout

--- Day-29/app.py
+++ Day-29/app.py
@@ -9,8 +9,29 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 logo = PhotoImage(file="./logo.png")
-canvas = Canvas(width=400, height=400)
-canvas.create_image(200, 200, image=logo)
-canvas.grid()
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=logo)
+canvas.grid(column=1, row=0)
+# Website Label
+website_label = Label(text="Website:").grid(column=0, row=1)
+# Website Entery
+website_input = Entry(width=35).grid(column=1, row=1, columnspan=2)
+
+
+# username Label
+username_label = Label(text="Email/Username:").grid(column=0, row=2)
+# username Entery
+username_input = Entry(width=35).grid(column=1, row=2, columnspan=2)
+
+
+# password Label
+password_label = Label(text="Password:").grid(column=0, row=3)
+# username Entery
+username_input = Entry(width=35).grid(column=1, row=3, columnspan=1)
+# Button generate Password
+pass_btn = Button(text="Generate Password").grid(column=3, row=3)
+
+# Add Button
+add_btn = Button(text="Add", width=35).grid(column=1, row=4)
 
 window.mainloop()
