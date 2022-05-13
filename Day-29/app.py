8641c1aac1f1b291138ea094d5b05bdaa5e6257e
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-13 22:24:52 +0100

    submit and save entred data in file

--- Day-29/app.py
+++ Day-29/app.py
@@ -4,6 +4,16 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def submit():
+    with open("passwords.txt", mode="a") as file:
+        file.write(
+            f"{website_input.get()} | {username_input.get()} | {password_input.get()}\n")
+
+        website_input.delete(0, END)
+        password_input.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -15,23 +25,27 @@ canvas.grid(column=1, row=0)
 # Website Label
 website_label = Label(text="Website:").grid(column=0, row=1)
 # Website Entery
-website_input = Entry(width=35).grid(column=1, row=1, columnspan=2)
-
+website_input = Entry(width=35)
+website_input.focus()
+website_input.grid(column=1, row=1, columnspan=2)
 
 # username Label
 username_label = Label(text="Email/Username:").grid(column=0, row=2)
 # username Entery
-username_input = Entry(width=35).grid(column=1, row=2, columnspan=2)
+username_input = Entry(width=35)
+username_input.grid(column=1, row=2, columnspan=2)
+username_input.insert(0, "abderyett@gmail.com")
 
 
 # password Label
 password_label = Label(text="Password:").grid(column=0, row=3)
-# username Entery
-username_input = Entry(width=35).grid(column=1, row=3, columnspan=1)
+# Password Entery
+password_input = Entry(width=35)
+password_input.grid(column=1, row=3, columnspan=1)
 # Button generate Password
 pass_btn = Button(text="Generate Password").grid(column=3, row=3)
 
 # Add Button
-add_btn = Button(text="Add", width=35).grid(column=1, row=4)
+add_btn = Button(text="Add", width=35, command=submit).grid(column=1, row=4)
 
 window.mainloop()
