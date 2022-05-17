ba04d97f50b9d10658612da1f1c2d6ec5bd7286a
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-17 06:56:55 +0100

    raising our exception

--- Day-30/main.py
+++ Day-30/main.py
@@ -1,22 +1,22 @@
-# 1- FileNotFound
-try:
-    file = open("Data.txt", "r")
-    a_dictionary = {"key": "value"}
-    value = a_dictionary["adfjkan"]
+# * 1- FileNotFound
+# try:
+#     file = open("Data.txt", "r")
+#     a_dictionary = {"key": "value"}
+#     value = a_dictionary["adfjkan"]
 
-except FileNotFoundError:
-    file = open("Data.txt", "w")
-    file.write("First writing on file")
-except KeyError as error_message:
-    print(f"That Key {error_message} doesn't exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.colse()
-    print("File closed")
+# except FileNotFoundError:
+#     file = open("Data.txt", "w")
+#     file.write("First writing on file")
+# except KeyError as error_message:
+#     print(f"That Key {error_message} doesn't exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.colse()
+#     print("File closed")
 
-# 2-  IndexError
+# * 2-  IndexError
 
 # fruits = ["banana", "orange", "apple"]
 # fruit = fruits[3]
@@ -26,7 +26,18 @@ finally:
 # a_dictionary = {"key": "value"}
 # value = a_dictionary["non_exist"]
 
-# 4- TypeError
+# * 4- TypeError
 
 # a = "abs"
 # print(a+5)
+
+
+# * Raise our own Error
+
+height = float(input("Height :"))
+weight = int(input("Weight :"))
+if height > 3:
+    raise ValueError(f"Human height should not be over {height} 🤨")
+
+bmi = weight/height**2
+print(bmi)
