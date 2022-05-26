62142fe6180201e530a58cbe44b7366c9425a47b
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-26 08:24:34 +0100

    Send daily quote via email

--- Day-32/app.py
+++ Day-32/app.py
@@ -1,12 +1,29 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email = 'abderyetttesting@hotmail.com'
 password = '123456789@()'
+day_quote = ''
 
-connection = smtplib.SMTP('smtp-mail.outlook.com')
-connection.starttls()
 
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs='ytra1001@hotmail.com', msg="Hello there!")
-connection.close()
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+
+
+with open("./quotes.txt") as file:
+    filecontents = file.readlines()
+    filecontents = [line.strip() for line in filecontents]
+    day_quote = random.choice(filecontents)
+
+
+print(day_quote)
+if day_of_week == now.weekday():
+    with smtplib.SMTP('smtp-mail.outlook.com') as connection:
+        connection.starttls()
+
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs='ytra1001@hotmail.com', msg=f"Subject: Quote of Day!\n\n{day_quote}")
