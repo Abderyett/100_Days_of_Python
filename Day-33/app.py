3bedd11fcc05e9964fbd1edd138b5e23feb9fa80
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-05-31 07:27:19 +0100

    start working with requests params

--- Day-33/app.py
+++ Day-33/app.py
@@ -1,13 +1,27 @@
+from urllib import response
 import requests
+MY_LATITUDE = 43.643930
+MY_LONGITUDE = -79.368970
 
+# response = requests.get(url='http://api.open-notify.org/iss-now.json')
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
+# # to Raise Error that could occur
+# response.raise_for_status()
+# data = response.json()
+# longitude = data['iss_position']['longitude']
+# latitude = data['iss_position']['latitude']
+# position = (latitude, longitude)
 
-# to Raise Error that could occur
+# print(position)
+
+parameters = {"lat": MY_LATITUDE, "lng": MY_LONGITUDE}
+
+response = requests.get(
+    url='https://api.sunrise-sunset.org/json', params=parameters)
 response.raise_for_status()
-data = response.json()
-longitude = data['iss_position']['longitude']
-latitude = data['iss_position']['latitude']
-position = (latitude, longitude)
 
-print(position)
+data = response.json()
+sunrise = data['results']['sunrise']
+sunset = data['results']['sunset']
+
+print(sunrise, sunset)
