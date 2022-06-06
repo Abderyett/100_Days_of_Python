06be5fdcf878fb90d886cdfeb506ca607c536a5a
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-06-06 07:46:41 +0100

    start working on UI

--- Day-34/ui.py
+++ Day-34/ui.py
@@ -1,2 +1,17 @@
+from tkinter import *
+
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, background=THEME_COLOR)
+        self.canvas = Canvas(width=300, height=250, background='white')
+        self.canvas.grid(row=1, column=1)
+        self.score_text = Label(
+            text="Score:", bg="white")
+        self.score_text.grid(row=0, column=1)
+        self.window.mainloop()
