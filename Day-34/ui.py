b03cd4cbf9ae8ad2dff31fcc2635b048f6e93923
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-06-07 07:54:40 +0100

    Complete Ui

--- Day-34/ui.py
+++ Day-34/ui.py
@@ -8,10 +8,23 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(padx=20, pady=20, background=THEME_COLOR)
-        self.canvas = Canvas(width=300, height=250, background='white')
-        self.canvas.grid(row=1, column=1)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         self.score_text = Label(
-            text="Score:", bg="white")
+            text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_text.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250,
+                             bg='white')
+        self.question_text = self.canvas.create_text(150, 125,
+                                                     text="Some dummy text", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        self.check_path = PhotoImage(file='./images/true.png')
+        self.valid_btn = Button(image=self.check_path, highlightthickness=0)
+        self.valid_btn.grid(row=2, column=0)
+
+        self.false_path = PhotoImage(file='./images/false.png')
+        self.wrong_btn = Button(image=self.false_path, highlightthickness=0)
+        self.wrong_btn.grid(row=2, column=1)
+
         self.window.mainloop()
