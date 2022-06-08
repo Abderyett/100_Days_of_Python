2ffc9be50b28f6b09a2d75ca25e3818f7bcb29f1
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-06-08 07:37:37 +0100

    ashowing questions in GUI

--- Day-34/ui.py
+++ Day-34/ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -15,7 +17,7 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250,
                              bg='white')
-        self.question_text = self.canvas.create_text(150, 125,
+        self.question_text = self.canvas.create_text(150, 125, width=280,
                                                      text="Some dummy text", fill=THEME_COLOR, font=("Arial", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
@@ -27,4 +29,10 @@ class QuizInterface:
         self.wrong_btn = Button(image=self.false_path, highlightthickness=0)
         self.wrong_btn.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
