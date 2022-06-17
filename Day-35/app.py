248a824f5fd779a38e0ff8bba4748a8397bff677
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-06-17 11:59:07 +0100

    start working with weather API

--- Day-35/app.py
+++ Day-35/app.py
@@ -2,13 +2,25 @@ import requests
 api_key = "619d37bdffe48dd4c39b272f2befe14b"
 location = "London"
 parameters = {
-    "lat": "40.741895",
-    "lon": "-73.989308",
-    "exclude": "current,minutely,hourly,alerts",
+    "lat": "36.5322102",
+    "lon": "2.9918496",
+    "exclude": "current,minutely,daily,alerts",
+    "lang": "fr",
     "appid": api_key,
 }
 
-data = requests.get(
+response = requests.get(
     f"https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data)
+# print(weather_data["hourly"][0]["weather"][0])
+sliced_list = weather_data["hourly"][0:13]
 
-print(data.json())
+will_rain = False
+for hour_data in sliced_list:
+    if hour_data["weather"][0]["id"] < 800:
+        will_rain = True
+
+if will_rain:
+    print("Bring umbrella")
