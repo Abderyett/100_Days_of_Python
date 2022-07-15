55bb4489d0000643cea8000a6afe86634133b5c0
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-07-15 07:26:32 +0100

    creating graph on pixela

--- Day-37/app.py
+++ Day-37/app.py
@@ -4,16 +4,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 pixela_token = os.getenv("PIXELA_TOKEN")
+username = os.getenv("USERNAME")
+token = os.getenv("TOKEN")
 
 pixela_enpoint = "https://pixe.la/v1/users"
 parameters = {
-    "token": "0005D1361555FCF73C77F610F9A6A1EA",
-    "username": "abderyett",
+    "token": token,
+    "username": username,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 
 
 }
-response = requests.post(url=pixela_enpoint, json=parameters)
+# response = requests.post(url=pixela_enpoint, json=parameters)
 
+# print(response.text)
+params = {
+    "id": "graph2",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ichou"
+}
+headers = {
+    "X-USER-TOKEN": token
+}
+
+graph_endpoint = f"{pixela_enpoint}/{username}/graphs"
+response = requests.post(url=graph_endpoint, json=params, headers=headers)
 print(response.text)
