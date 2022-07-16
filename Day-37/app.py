5ae3b9a1137fdbf11c48e1eb04657fe449921ba9
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-07-16 07:17:59 +0100

    Post pixel

--- Day-37/app.py
+++ Day-37/app.py
@@ -7,7 +7,7 @@ pixela_token = os.getenv("PIXELA_TOKEN")
 username = os.getenv("USERNAME")
 token = os.getenv("TOKEN")
 
-pixela_enpoint = "https://pixe.la/v1/users"
+pixela_endpoint = "https://pixe.la/v1/users"
 parameters = {
     "token": token,
     "username": username,
@@ -30,6 +30,20 @@ headers = {
     "X-USER-TOKEN": token
 }
 
-graph_endpoint = f"{pixela_enpoint}/{username}/graphs"
-response = requests.post(url=graph_endpoint, json=params, headers=headers)
+graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+# response = requests.post(url=graph_endpoint, json=params, headers=headers)
+# print(response.text)
+
+
+# post pixel
+create_pixel_params = {
+    "date": "20220716",
+    "quantity": "22"
+
+}
+post_pixel_endpoint = f"{pixela_endpoint}/{username}/graphs/graph2"
+print(post_pixel_endpoint)
+response = requests.post(
+    url=post_pixel_endpoint, json=create_pixel_params, headers=headers)
+
 print(response.text)
