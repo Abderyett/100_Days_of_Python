7c6e89d60309669cf94b3ad691287950966fc38a
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-07-29 22:04:01 +0100

    start working with webscraping

--- Day-38/app.py
+++ Day-38/app.py
@@ -6,13 +6,24 @@ load_dotenv()
 app_id = os.getenv("NUTRITION_ID")
 api_key = os.getenv("NUTRITION_IX_API_KEY")
 
+
 endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 params = {
-    "query": "ran 2 miles"
+    "query": input("Tell me which excercises you did: "),
+    "gender": "male",
+    "weight_kg": 80,
+    "height_cm": 190,
+    "age": 34
+
 }
 
 headers = {"x-app-id": app_id, "x-app-key": api_key}
 
 response = requests.post(url=endpoint, json=params, headers=headers)
 
-print(response)
+data = response.json()
+
+print(type(data))
+print(data)
+
+# ran 5K and cycled for 40 minutes.
