b67f3a17c5d69a1d801056024281b12436ec8248
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-08-21 06:57:24 +0100

    finding and selecting with beautiful soup

--- Day-45/app.py
+++ Day-45/app.py
@@ -1,3 +1,4 @@
+from distutils.filelist import findall
 from bs4 import BeautifulSoup
 
 
@@ -7,4 +8,27 @@ soup = BeautifulSoup(contents, 'html.parser')
 
 # print(soup.title.string)
 # print(soup.title.name)
-print(soup.prettify())
+# print(soup.prettify())
+
+all_anchers_tags = soup.find_all(name="a")
+
+for tag in all_anchers_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
+    pass
+
+header = soup.find(name='h1', id='name')
+
+# print(header)
+
+h3_heading = soup.find_all(name='h3', class_='heading')
+
+print(h3_heading)
+
+name = soup.select_one('#name')
+
+print(name)
+
+headings = soup.select('.heading')
+
+print(headings)
