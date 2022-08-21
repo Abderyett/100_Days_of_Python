d54b769f6014add8d060b3eb9fed206b0c5338d9
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2022-08-21 07:47:52 +0100

    start scraping live website

--- Day-45/app.py
+++ Day-45/app.py
@@ -1,34 +1,46 @@
-from distutils.filelist import findall
+
 from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, 'html.parser')
+
+link_text = soup.find_all(name='a')
+
+for link in link_text:
+    print(link.getText())
 
 
-with open('./website.html') as file:
-    contents = file.read()
-soup = BeautifulSoup(contents, 'html.parser')
+# with open('./website.html') as file:
+#     contents = file.read()
+# soup = BeautifulSoup(contents, 'html.parser')
 
-# print(soup.title.string)
-# print(soup.title.name)
-# print(soup.prettify())
+# # print(soup.title.string)
+# # print(soup.title.name)
+# # print(soup.prettify())
 
-all_anchers_tags = soup.find_all(name="a")
+# all_anchers_tags = soup.find_all(name="a")
 
-for tag in all_anchers_tags:
-    # print(tag.getText())
-    # print(tag.get("href"))
-    pass
+# for tag in all_anchers_tags:
+#     # print(tag.getText())
+#     # print(tag.get("href"))
+#     pass
 
-header = soup.find(name='h1', id='name')
+# header = soup.find(name='h1', id='name')
 
-# print(header)
+# # print(header)
 
-h3_heading = soup.find_all(name='h3', class_='heading')
+# h3_heading = soup.find_all(name='h3', class_='heading')
 
-print(h3_heading)
+# print(h3_heading)
 
-name = soup.select_one('#name')
+# name = soup.select_one('#name')
 
-print(name)
+# print(name)
 
-headings = soup.select('.heading')
+# headings = soup.select('.heading')
 
-print(headings)
+# print(headings)
